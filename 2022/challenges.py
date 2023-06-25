c4c703584664b4dc329882cd82094a3987d7ae7d
Dahmon Bicheno <dahmon.bicheno@gmail.com>
2023-06-25 14:34:08 +1000

    feat: Completed day 1 part 2

--- 2022/challenges.py
+++ 2022/challenges.py
@@ -6,3 +6,12 @@ def part_one():
     groups_sum = sum_groups(groups_int)
 
     return max(groups_sum)
+
+def part_two():
+    input = read_input()
+    groups_int = extract_groups_int(input)
+    groups_sum = sum_groups(groups_int)
+    groups_sorted = sorted(groups_sum)
+    last_three = groups_sorted[-3:]
+
+    return sum(last_three)
--- 2022/test_challenges.py
+++ 2022/test_challenges.py
@@ -1,17 +1,25 @@
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 import unittest
-from challenges import part_one
+from challenges import part_one, part_two
 
 mock_input = """10
 20
 30
+
+5
+15
+20
 
 30
 50
 20
 
 10
-50"""
+50
+
+60
+10
+10"""
 
 class TestChallenges(unittest.TestCase):
     @patch('challenges.read_input')
@@ -24,6 +32,17 @@ class TestChallenges(unittest.TestCase):
 
         # ASSERT
         self.assertEqual(result, 100)
+
+    @patch('challenges.read_input')
+    def test_part_two(self, mock_read_input):
+        # ARRANGE
+        mock_read_input.return_value = mock_input
+
+        # ACT
+        result = part_two()
+
+        # ASSERT
+        self.assertEqual(result, 240)
 
 if __name__ == '__main__':
     unittest.main()
