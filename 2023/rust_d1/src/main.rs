c657b752ed5fc80b603338c009c6fa17d561d964
Dahmon Bicheno <dahmon.bicheno@gmail.com>
2024-01-07 16:51:06 +1100

    feat(2023): Complete 2023 Day 1 Pt. 2

--- 2023/rust_d1/src/main.rs
+++ 2023/rust_d1/src/main.rs
@@ -1,8 +1,21 @@
 use regex::Regex;
+use std::borrow::Borrow;
+use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Lines};
 
-fn part_one(lines: Lines<BufReader<File>>) -> Result<u32, regex::Error> {
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+fn part_one() -> Result<u32, regex::Error> {
+    let file = File::open("input.txt").unwrap();
+    let lines = io::BufReader::new(file).lines();
+
     let first_re = Regex::new(r"^[a-z]*(\d)")?;
     let last_re = Regex::new(r"(\d)[a-z]*$")?;
 
@@ -40,14 +53,60 @@ fn part_one(lines: Lines<BufReader<File>>) -> Result<u32, regex::Error> {
     Ok(total)
 }
 
-fn main() {
+fn part_two() -> Result<u32, Box<dyn Error>> {
     let file = File::open("input.txt").unwrap();
-
     let lines = io::BufReader::new(file).lines();
 
-    let part_one_result = part_one(lines);
+    let str_nums: [&str; 10] = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    let mut total: u32 = 0;
+
+    for line in lines {
+        let line = line.unwrap();
+        let mut first: Option<String> = None;
+        let mut last: Option<String> = None;
+
+        for (index, char) in line.chars().enumerate() {
+            if char.is_digit(10) {
+                match first {
+                    Some(_) => last = Some(char.to_string()),
+                    None => first = Some(char.to_string()),
+                }
+            }
+
+            for (num_index, str_num) in str_nums.iter().enumerate() {
+                if line[index..].starts_with(str_num) {
+                    match first {
+                        Some(_) => last = Some(num_index.to_string()),
+                        None => first = Some(num_index.to_string()),
+                    }
+                }
+            }
+        }
+
+        let first = first.unwrap();
+        let num: String = match last {
+            Some(last) => format!("{}{}", first, last),
+            None => format!("{}{}", first, first),
+        };
+
+        total += num.parse::<u32>()?;
+    }
+
+    Ok(total)
+}
+
+fn main() {
+    let part_one_result = part_one();
     match part_one_result {
         Ok(result) => println!("Part One Result: {}", result),
         Err(error) => println!("Part One Error: {}", error),
+    }
+
+    let part_two_result = part_two();
+    match part_two_result {
+        Ok(result) => println!("Part Two Result: {}", result),
+        Err(error) => println!("Part Two Error: {}", error),
     }
 }
